fc6647efbe8c0e7b494542b6d03a5c7480ad1a8a
meteoorkip <svenkonings@live.nl>
2017-02-07 01:58:09 +0100

    Add fullscreen

--- src/main/scala/dj/plug/plugdj/player/PlayerActivity.scala
+++ src/main/scala/dj/plug/plugdj/player/PlayerActivity.scala
@@ -1,9 +1,12 @@
 package dj.plug.plugdj.player
 
 import android.content.Intent
-import android.os.Bundle
+import android.content.res.Configuration
+import android.content.res.Configuration.ORIENTATION_LANDSCAPE
+import android.os.{Build, Bundle}
 import android.support.v4.app.ActivityCompat
 import android.support.v7.app.AppCompatActivity
+import android.view.View._
 import dj.plug.plugdj.MainApplication.playerService
 import dj.plug.plugdj.rooms.RoomActivity
 import dj.plug.plugdj.{TR, TypedViewHolder}
@@ -46,4 +49,27 @@ class PlayerActivity extends AppCompatActivity with ServiceListener {
   }
 
   override def stop(): Unit = onBackPressed()
+
+  override def onWindowFocusChanged(hasFocus: Boolean): Unit = {
+    super.onWindowFocusChanged(hasFocus)
+    updateConfiguration(getResources.getConfiguration)
+  }
+
+  override def onConfigurationChanged(newConfig: Configuration): Unit = {
+    super.onConfigurationChanged(newConfig)
+    updateConfiguration(newConfig)
+  }
+
+  private def updateConfiguration(configuration: Configuration): Unit = {
+    var flags = SYSTEM_UI_FLAG_LAYOUT_STABLE
+    if (configuration.orientation == ORIENTATION_LANDSCAPE) flags |= {
+      if (Build.VERSION.SDK_INT >= 19) SYSTEM_UI_FLAG_IMMERSIVE_STICKY |
+        SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION |
+        SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN |
+        SYSTEM_UI_FLAG_HIDE_NAVIGATION |
+        SYSTEM_UI_FLAG_FULLSCREEN
+      else SYSTEM_UI_FLAG_LOW_PROFILE
+    }
+    viewHolder.rootView.setSystemUiVisibility(flags)
+  }
 }
